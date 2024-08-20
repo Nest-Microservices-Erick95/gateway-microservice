5eaa39f94c0386e0fe9ab1c6279498e0b80033e1
ErickOlivarr <131342527+ErickOlivarr@users.noreply.github.com>
2024-08-20 00:41:56 -0600

    health-check

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { RpcCustomExceptionFilter } from './common/exceptions/rpc-custom-exception.filter';
 
 async function bootstrap() {
@@ -9,7 +9,12 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule); 
 
-  app.setGlobalPrefix('api');
+  //lo del exclude del setGlobalPrefix se puso por el tema de google cloud con kubernetes, lo ultimo del curso, se explica de esto en la NOTA.txt que está en la carpeta de products-launcher del curso, pero pues es para que ese prefijo de /api excluya al endpoint de '', osea al endpoint raiz  con el metodo GET, ese endpoint lo tenemos en el modulo de health-check de este gateway-ms
+  app.setGlobalPrefix('api', {
+    exclude: [
+      { path: '', method: RequestMethod.GET }
+    ]
+  });
 
   app.useGlobalPipes(  
     new ValidationPipe({ 
@@ -21,6 +26,6 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter()); 
 
   await app.listen(envs.port);
-  logger.log(`Gateway running on port ${envs.port}`);
+  logger.log(`Gateway running on port ${envs.port} with health-check`);
 }
 bootstrap();
